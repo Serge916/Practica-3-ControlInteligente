085eccfa7f306db1c406971a05eed34f8e175c66
SergioGeronimoParras <sergio.g.p@hotmail.es>
2022-11-25 13:53:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import localizacion as loc
+import plots
+
+dist = 200    # (m) este parámetro no se cambia
+t_max = 5000  # (s) este parámetro no se cambia
+
+#seeds = [123456789, 234567891, 34567891, 456789123, 567891234, 67891234, 7891234, 89123456, 912345678, 213456789]
+seeds = [123456789]
+
+q = 0.2 ** 2  # prueba con distinto ruido odométrico, varianza (m2)
+r = 0.1 ** 2  # prueba con distinto ruido sensorial, varianza (m2)
+rango_sensor = 5  # prueba con distinto alcance sensorial (m)
+
+# Localización por odometría
+estados1 = loc.localizacion_odometrica(q, r, rango_sensor, dist, t_max, seeds[0])
+plots.plot_estados(estados1, "localizacion_odometrica.pdf", dist)
+
+# Localización con FK y un landmark
+estados2 = loc.localizacion_FK(q, r, rango_sensor, dist, t_max, seeds)
+estados2_media = sum(estados2)/len(estados2)
+plt.figure(2)
+plots.plot_estados(estados2_media, "localizacion_FK.pdf", dist)
---
+++ plots.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+
+def plot_estados(estados, file=None, ymax=None):
+    fig, axes = plt.subplots(5, 1)
+
+    axes[0].plot(estados['x'], linewidth=0.8)
+    axes[0].plot(estados['xest'], linewidth=0.8)
+    axes[0].set_ylabel('x (azul)\nx_est (rojo)')
+    if (ymax is not None):
+        axes[0].set_ylim(-ymax * 0.05, ymax * 1.05)
+
+    axes[1].set_ylabel('xerror')
+    axes[1].plot(estados['xerror'], linewidth=0.8)
+
+    axes[2].set_ylabel('Z')
+    axes[2].plot(estados['z'], linewidth=0.8)
+    axes[2].set_ylim(-0.9)
+
+    axes[3].set_ylabel('K')
+    axes[3].plot(estados['K'], linewidth=0.8)
+    axes[3].set_ylim(0, 1)
+
+    axes[4].set_ylabel('P')
+    axes[4].plot(estados['p'], linewidth=0.8)
+
+    for i in range(5):
+        axes[i].spines['top'].set_visible(False)
+        axes[i].spines['right'].set_visible(False)
+        axes[i].grid(linestyle=':', linewidth=0.3)
+        if (i < 4):
+            axes[i].set_xticklabels([])
+
+    if (file is None):
+        plt.show()
+    else:
+        plt.savefig(file)
+
+
