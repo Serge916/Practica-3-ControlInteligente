d66eecabc875aff8ed006e53b9f34253fdd090f5
SergioGeronimoParras <sergio.g.p@hotmail.es>
2022-12-08 11:58:38 +0100

    Creo log de medias en csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import localizacion as loc
 import plots
+import csv
 
 dist = 200    # (m) este parámetro no se cambia
 t_max = 5000  # (s) este parámetro no se cambia
@@ -11,17 +12,23 @@ seeds = [123456789]
 q_set = [0.2, 0.4, 0.5, 0.7]
 r_set = [0.2, 0.3, 0.4, 0.8]
 
-for i in enumerate(q_set):
-    q = i[1] ** 2  # prueba con distinto ruido odométrico, varianza (m2)
-    r = r_set[i[0]] ** 2  # prueba con distinto ruido sensorial, varianza (m2)
-    rango_sensor = 5  # prueba con distinto alcance sensorial (m)
+with open('log.csv','w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['x', 'xest', 'xerror', 'p', 'z', 'K'])
 
-    # Localización por odometría
-    estados1 = loc.localizacion_odometrica(q, r, rango_sensor, dist, t_max, seeds[0])
-    plots.plot_estados(estados1, i[1], r_set[i[0]], "media/lo_"+ str(i[0]) +".pdf", dist)
+    for i in enumerate(q_set):
+        q = i[1] ** 2  # prueba con distinto ruido odométrico, varianza (m2)
+        r = r_set[i[0]] ** 2  # prueba con distinto ruido sensorial, varianza (m2)
+        rango_sensor = 5  # prueba con distinto alcance sensorial (m)
 
-    # Localización con FK y un landmark
-    estados2 = loc.localizacion_FK(q, r, rango_sensor, dist, t_max, seeds)
-    estados2_media = sum(estados2)/len(estados2)
-    plt.figure(2)
-    plots.plot_estados(estados2_media, i[1], r_set[i[0]], "media/lfk"+ str(i[0]) +".pdf", dist)
+        # Localización por odometría
+        estados1 = loc.localizacion_odometrica(q, r, rango_sensor, dist, t_max, seeds[0])
+        plots.plot_estados(estados1, i[1], r_set[i[0]], "media/lo_"+ str(i[0]) +".pdf", dist)
+        writer.writerow(estados1.mean())
+
+        # Localización con FK y un landmark
+        estados2 = loc.localizacion_FK(q, r, rango_sensor, dist, t_max, seeds)
+        writer.writerow(estados2[0].mean())
+        estados2_media = sum(estados2)/len(estados2)
+        plt.figure(2)
+        plots.plot_estados(estados2_media, i[1], r_set[i[0]], "media/lfk"+ str(i[0]) +".pdf", dist)
