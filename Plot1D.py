e4344201795e3dd2b00a952731e39c674afda79a
Juanjo12022001 <119055897+Juanjo12022001@users.noreply.github.com>
2022-12-11 09:50:30 +0100

    Mapa de calor que representa la correlación entre las diferentes variables

--- Plot1D.py
+++ Plot1D.py
@@ -1,1 +1,21 @@
 #Programa para plotear distintos valores de ruido.
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+#Cargamos los datos que se han recogido en el csv generado
+df = pd.read_csv('C:/Users/juanj/OneDrive/Escritorio/Practica-3/log.csv')
+
+#Configuramos el mapa de calor
+plt.figure(figsize=(12,10), dpi= 80)
+sns.heatmap(df.corr(), xticklabels = df.corr().columns, yticklabels = df.corr().columns, cmap = 'RdYlGn', center = 0, annot = True)
+
+#Decoración argumento
+plt.title("Correlación entre las variables", fontsize = 22)
+plt.xticks(fontsize = 12)
+plt.yticks(fontsize = 12)
+plt.show()
+
+
+
+
