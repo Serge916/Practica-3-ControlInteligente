6b5e8ac6f0868177e6679a63d97ca8ba7227200a
SergioGeronimoParras <sergio.g.p@hotmail.es>
2022-12-16 16:49:15 +0100

    Algoritmo 2D funcional

--- TwoDim/localizacion2.py
+++ TwoDim/localizacion2.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 q = 0.3 ** 2
-r = 0
-rango_sensor = 200
+r = 0.06 ** 2
+rango_sensor = 100
 t_max = 5000
 
 estados = []
 
 #Constantes iniciales
 v_lin = 0.5     #Velocidad lineal m/s
-radio = 700
+radio = 260
 v_ang = v_lin / radio     #Velocidad angular rad/s
 x = np.array([[0], [0]], dtype=float)    #Posición inicial 0,0
 ang = ang_id = ang_est= 0
@@ -43,12 +43,14 @@ K = np.array([[0, 0],
               [0, 0]])  # 2x2 para (x,y)
 
 # Landmarks definidos respecto del origen
-m = [ (0, 2*radio), 
-      (radio, radio - 50)]
+m = [ (0, 2*radio),
+        (0, 0)]
 
 #estados.append(pd.DataFrame(data={'x': [], 'xest': [], 'xerror': [], 'p': [], 'z': [], 'K': [], 'q': [], 'r': []}))
-estados = pd.DataFrame(estados.append(pd.DataFrame(data = {'x_est':[], 'x': []})))
+estados = pd.DataFrame(estados.append(pd.DataFrame(data = {'x_est':[], 'x': [], 'err': [], 'p': [], 'z': [], 'k': []})))
 
+
+flag = False
 #Simulación
 for t in range(t_max):
     #Calculo los números aleatorios
@@ -73,37 +75,53 @@ for t in range(t_max):
     for mi in m:
         if ( math.fabs(( math.sqrt( (mi[0] - x[0][0])**2 + (mi[1] - x[1][0])**2) ) ) < rango_sensor):  # si se detecta el landmark
             # Actualizamos la observación (con ruido)
-            foo = np.array([[x[0][0] + r2 * (r**0.5)], [0]])
+            foo = np.array([[x[0][0] + r2 * (r**0.5)], [x[1][0] + r2 * (r**0.5)]])
+            if not flag: 
+                flag = True 
             correccion = True
-        else:
-            foo = np.array([[math.nan], [0]])
+        else:   
+            foo = np.array([[math.nan], [math.nan]])
         z.append(foo)
 
     # Corrección basada en z, sólo si se detectó algún landmark
     if (correccion):
         # Actualización de la medición (innovación)
-        innov = points = 0
+        points = 0
+        innov = np.array([[0], [0]], dtype=float)
         for zi in z:
             if not math.isnan(zi[0]):
-                innov = innov + zi - H * x_est
+                innov = innov + zi - H @ x_est
                 points += 1
-        innov /= points         #Hago la media
+        innov = innov / points         #Hago la media
 
         # Ganancia de Kalman
-        K = P * np.transpose(H) * inv(H * P * np.transpose(H) + R)
-
+        K = P @ np.transpose(H) @ inv(H @ P @ np.transpose(H) + R)
+        """
+        if flag:
+            print("x_est1: ", x_est)
+        """
         # Estimación a posteriori
-        x_est = x_est + K * innov
+        x_est = x_est + K @ innov
 
         # Covarianza del error asociada a la estimación a posteriori
         P = P - K * H * P
     else:
         K = np.array([[math.nan, 0],
-                        [ 0, 0]])  # 2x2 para (x,y)
+                        [ 0, math.nan]])  # 2x2 para (x,y)
 
-    fila = {'x_est': [(x_est[0][0], x_est[1][0])], 'x': [(x[0][0], x[1][0])]}
+    fila = {'x_est': [(x_est[0][0], x_est[1][0])], 'x': [(x[0][0], x[1][0])], 'err':[math.fabs(x[1][0]-x_est[1][0]+x[0][0]-x_est[0][0])],
+             'p': [(P[0][0], P[1][1])], 'k': [(K[0][0], K[1][1])], 'z': [(z[0][0], z[1][0])]}
     estados = pd.concat([estados, pd.DataFrame(fila)], ignore_index=True )
-
+    """
+    if flag:
+        print("x_est2: ",x_est)
+        print("foo: ",foo)
+        print("K: ",K)
+        print("P: ",P)
+        print("x_real: ",x)
+        print("innov: ", innov)
+        wait = input("Press enter to continue...")
+    """
 
 disp = plt.figure(num="MovimientoCircular",figsize=(5,5))
 c,v = zip(*estados['x_est'])
@@ -111,11 +129,12 @@ plt.plot(c, v, linewidth=1 , linestyle='dashed' , label="Estimado")
 c,v = zip(*estados['x'])
 plt.plot(c, v, linewidth=1 , label="Real")
 
+#Circunferencia de radio = rango_sensor
 angles = np.linspace(0, 2*math.pi, 150)
 a = np.array([rango_sensor * math.cos(angle) for angle in angles])
 b = np.array([rango_sensor * math.sin(angle) for angle in angles])
 
-
+#Muevo la circunferencia a cada uno de los landmarks
 for mi in m:
     ai = a + mi[0]
     bi = b + mi[1]
@@ -125,4 +144,46 @@ plt.plot(m[0][0], m[0][1], "x", color='green', label="Landmark")
 
 plt.legend(loc='upper left')
 plt.title("Parte 2")
+plt.show()
+
+plt.close()
+
+fig, axes = plt.subplots(6,1)
+timesim = list(range(0,t_max))
+c1,v1 = zip(*estados['x'])
+c,v = zip(*estados['x_est'])
+axes[0].plot(timesim, c1, linewidth=0.8)
+axes[0].plot(timesim, c, linewidth=0.8)
+axes[0].set_ylabel('x (azul)\ x_est (rojo)')
+
+axes[1].plot(timesim, v1, linewidth=0.8)
+axes[1].plot(timesim, v, linewidth=0.8)
+axes[1].set_ylabel('y (azul)\ y_est (rojo)')
+
+axes[2].set_ylabel('err')
+axes[2].plot(estados['err'], linewidth=0.8)
+
+axes[3].set_ylabel('Z')
+c,v = zip(*estados['z'])
+axes[3].plot(timesim, c, linewidth=0.8)
+axes[3].plot(timesim, v, linewidth=0.8)
+
+axes[4].set_ylabel('K')
+c,v = zip(*estados['k'])
+axes[4].plot(timesim, c, linewidth=0.8)
+axes[4].plot(timesim, v, linewidth=0.8)
+#axes[3].set_ylim(0, 1)
+
+axes[5].set_ylabel('P')
+c,v = zip(*estados['p'])
+axes[5].plot(timesim, c, linewidth=0.8)
+axes[5].plot(timesim, v, linewidth=0.8)
+
+for i in range(5):
+    axes[i].spines['top'].set_visible(False)
+    axes[i].spines['right'].set_visible(False)
+    axes[i].grid(linestyle=':', linewidth=0.3)
+    if (i < 4):
+        axes[i].set_xticklabels([])
+
 plt.show()
